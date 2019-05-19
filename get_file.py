b00a5e4db1be87930aa53d2808087a8b885ad8cf
ogspeace <ogremagne@gmail.com>
2019-05-19 08:55:55 +0800

    added reverse-progress bar

--- get_file.py
+++ get_file.py
@@ -17,6 +17,30 @@ def print_stdout(s):
     print(s)
     sys.stdout.flush()
 
+
+def get_terminal_size():
+    rows, columns = os.popen('stty size', 'r').read().split()
+    return int(rows), int(columns)
+
+def display_progress_bar(bytes_received, filesize, ch='*', scale=0.55):
+    _, columns = get_terminal_size()
+    max_width = int(columns * scale)
+
+    filled = int(round(max_width * bytes_received / float(filesize)))
+    remaining = max_width - filled
+    bar = ch * filled + ' ' * remaining
+    percent = round(100.0 * bytes_received / float(filesize), 1)
+    text = ' > |{bar}| {percent} \r'.format(bar=bar, percent=percent)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+def progress_Check(stream = None, chunk = None, file_handle = None, remaining = None):
+    file_size = stream.filesize
+    bytes_received = file_size - remaining
+#    percent = (100*(file_size-remaining))/file_size
+    display_progress_bar(remaining, file_size)
+#    print_stdout("%.2f downloaded"%percent)
+
 dir_name = os.path.dirname(os.path.realpath(__file__))
 
 t = open(dir_name+"/to_dl.txt","r")
@@ -26,7 +50,7 @@ if not os.path.exists(dir_name+"/vids_dir/"):
     os.makedirs(dir_name+"/vids_dir/")
 for v in toDL_list:
     try:
-        yt = YouTube(v.strip())
+        yt = YouTube(v.strip(), on_progress_callback=progress_Check)
         print_stdout("[%s] Downloading video : '%s'. . ."%(cnt,yt.title))
         yt_filt = yt.streams.filter(progressive=True, file_extension='mp4')
         for x in yt_filt.all():
